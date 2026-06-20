1821e80140979f60211368ddd0ba599d4b20a335
Yadvesh yadav <118018384+yadveshyadav@users.noreply.github.com>
2026-06-20 15:05:24 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,1 +1,7 @@
 # auto-generated placeholder
+#!/usr/bin/env python
+import os
+from app import create_app
+
+# Create the WSGI application object for the web server (e.g., gunicorn)
+application = create_app(os.getenv('FLASK_ENV', 'production'))
